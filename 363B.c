6210e5734a9d8ee1ffa74d77dda0cf4b1bde422a
Procsima <evseev139@yandex.ru>
2023-04-22 02:03:27 +0300

    Full solution

--- 363B.c
+++ 363B.c
@@ -1,28 +1,67 @@
 //https://codeforces.com/contest/363/problem/B
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
+typedef struct pair
+{
+    int first;
+    int second;
+} pair;
+
+typedef struct queue
+{
+    int *arr;
+    int sum;
+    int start;
+    pair min;
+} queue;
+
+void
+queue_ctor(queue *this, int n, int k)
+{
+    this->arr = calloc(n, sizeof(int));
+    this->sum = 0;
+    for (int i = 0; i < k; ++i) {
+        scanf("%d", &this->arr[i]);
+        this->sum += this->arr[i];
+    }
+    this->start = 0;
+    this->min.first = this->sum;
+    this->min.second = 0;
+}
+
+void
+queue_step(queue *this, int k)
+{
+    scanf("%d", &this->arr[this->start + k]);
+    this->sum = this->sum - this->arr[this->start] + this->arr[this->start + k];
+    if (++this->start, this->min.first > this->sum) {
+        this->min.first = this->sum;
+        this->min.second = this->start;
+    }
+}
+
+void
+queue_dtor(queue *this)
+{
+    free(this->arr);
+}
+
+int
+main(void)
+{
     int n, k;
     scanf("%d%d", &n, &k);
-    int arr[n];
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &arr[i]);
-    }
 
-    int sum = 0;
-    for (int i = 0; i < k; ++i) {
-        sum += arr[i];
-    }
-
-    int x = sum, f = 0;
+    queue q;
+    queue_ctor(&q, n, k);
 
     for (int i = 0; i < n - k; ++i) {
-        if (x > (x + arr[k + i] - arr[i])) {
-            f = i;
-            x = x + arr[k + i] - arr[i];
-        }
+        queue_step(&q, k);
     }
-    printf("%d\n", f == 0 ? 1 : ++f);
-    //printf("%d", x);
+
+    printf("%d\n", q.min.second + 1);
+
+    queue_dtor(&q);
 }
