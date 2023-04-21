0b937a45ad08bdae71e1cee7c907d364476e97dd
Procsima <evseev139@yandex.ru>
2023-04-22 00:16:18 +0300

    Full solution

--- 1721B.c
+++ 1721B.c
@@ -2,20 +2,40 @@
 
 #include <stdio.h>
 
-int main(void) {
-    int t;
-    scanf("%d", &t);
-    int n, m, x, y, d;
+int n, m, x, y, d;
 
-    for (int i = 0; i < t; ++i) {
+int
+touch(int w)
+{
+    switch (w) {
+        case 1:
+            return y - d <= 1;
+        case 2:
+            return x + d >= n;
+        case 3:
+            return y + d >= m;
+        case 4:
+            return x - d <= 1;
+        default:
+            return w / 0;
+    }
+}
+
+int
+main(void)
+{
+    int _t;
+    scanf("%d", &_t);
+
+    while (_t-- > 0) {
         scanf("%d%d%d%d%d", &n, &m, &x, &y, &d);
-        if ((x + d < m) && (x - d > 1) && (y - d > 1) && (y + d < n)) {
-            printf("-1");
-        } else {
-            printf("%d\n", m + n - 2);
-        }
+
+        printf("%d\n", touch(1) && touch(4) ||
+                       touch(2) && touch(3) ||
+                       touch(1) && touch(3) ||
+                       touch(2) && touch(4) ?
+                       -1 :
+                       m + n - 2);
     }
 
 }
-//(x - 1 <= d) && (m - y <=d) && (n - x <=d) && (y - 1 <= d)
-//(x + d < m) && (x - d > 1) && (y - d > 1) && (y + d < n)
